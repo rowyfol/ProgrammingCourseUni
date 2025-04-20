5f1fd762c5c5627d067ce37d5dd0ca8cc88becd2
Ashkan Namjoo <aejooni@proton.me>
2025-04-20 18:41:15 +0330

    Update E1.cpp

--- E1.cpp
+++ E1.cpp
@@ -2,47 +2,58 @@
 
 using namespace std;
 
+void space()
+{
+        cout<<endl;
+}
+
 int main()
 {
-	int loop_n;
-	cout<<"Enter loop_n: "<<endl;
-	cin>>loop_n;
+        int loop_n;
+        cout<<"Enter loop_n: ";
+        cin>>loop_n;
+        space();
 
-	int a,b, max, min;
+        int a, b, max, min;
 
-	cout<<"Enter the first number:"<<endl;
-	cin>>b;
+        cout<<"Enter the first number:";
+        cin>>b;
+        space();
 
-	cout<<"Enter the second number:"<<endl;
-	cin>>a;
+        cout<<"Enter the second number:";
+        cin>>a;
+        space();
 
-	if (a>=b) {
-		max = a;
-	} else {
-		min = a;
-	}
+        if (a >= b) {
+                max = a;
+                min = b;
+
+        } else {
+                min = a;
+                max = b;
+        }
+
+        int input_n;
+
+        for (int i=0; i<loop_n;i++)
+        {
+                cout<<"Enter the number: ";
+                cin>>input_n;
+                space();
+
+                if (input_n > max)
+                {
+                        max = input_n;
+                }
+                if (input_n < min)
+                {
+                        min =input_n;
+                }
+        }
+
+        cout<<"Max: "<<max<<endl;
+        cout<<"Min: "<<min<<endl;
 
 
-	int input_n;
-
-	for (int i=0; i<loop_n;i++)
-	{
-		cout<<"Enter the number: "<<endl;
-		cin>>input_n;
-
-		if (input_n>max)
-		{
-			max = input_n;
-		}
-		if (input_n<min)
-		{
-			min =input_n;
-		}
-	}
-
-	cout<<"Max: "<<max<<endl;
-	cout<<"Min: "<<min<<endl;
-
-
-	return 0;
+        return 0;
 }
